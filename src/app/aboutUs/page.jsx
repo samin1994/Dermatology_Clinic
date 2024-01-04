7648c7acac6560bbad67c51d3f5724476d70ff69
samin1994 <saminlabbaf1994@gmail.com>
2024-01-04 12:32:56 +0330

    added detail of cv in aboutUs

--- src/app/aboutUs/page.jsx
+++ src/app/aboutUs/page.jsx
@@ -5,13 +5,20 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Container } from '@mui/system'
 import styles from './page.module.css'
+import { motion } from 'framer-motion';
 
 
 const AboutUs = () => {
 
+  const duration = 1000; // ms
+const delay = 500; // ms
+var i = 0
+
+const animStr = (i) => `fadeIn ${duration}ms ease-out ${delay * (i + 1)}ms forwards`;
+
   // console.log(inputValues)
   return (
-    <Container>
+    <Container sx={{pb: '70px'}}>
       <Box className={styles.main}>
         <Box className={styles.imgContainer}>
           <Image className={styles.bio_image} width={300} height={300} src='/bio_image.png' sizes={'100vw'} alt='dr negin safdarian'/>
@@ -29,7 +36,50 @@ const AboutUs = () => {
           </Typography>
         </Box>
       </Box>
+      <Box className={styles.bioDetails}>
+        <Typography className={styles.bioTitle} variant='h6' color='text.secondary'>تحصیلات دانشگاهی</Typography>
+        <Typography variant='number'>رتبه سوم بورد تخصصی پوست ایران در سال  1376 از دانشگاه علوم پزشکی تهران</Typography>
+        <Typography variant='number'>پزشکی عمومی از دانشگاه علوم پزشکی تهران - سال 1364 تا 1370</Typography>
 
+        <Typography className={styles.bioTitle} variant='h6' color='text.secondary'>تجربیات کاری</Typography>
+        <Typography variant='number'>23 سال تجربه کاری در زمینه درماتولوژی (تخصص پوست و مو)</Typography>
+        <Typography variant='number'>دارای تجربه در تمامی انواع لیزرهای درماتولوژی</Typography>
+        <Typography variant='body1' mt={1} fontWeight='600'>تجربه تدریس :</Typography>
+        <Typography variant='number'>تدریس دروس درماتولوژی به دانشجویان پزشکی به مدت 7 سال</Typography>
+        <Typography variant='body1' mt={1} fontWeight='600'>محل‌های طبابت به طور حرفه ای : </Typography>
+        <Typography variant='number'>مطب و کلینیک شخصی</Typography>
+        <Typography variant='number'>کلینیک تخصصی پوست و مو به سیما</Typography>
+        <Typography variant='body1' mt={1} fontWeight='600'>علاقه اصلی : </Typography>
+        <Typography variant='number'>درماتولوژی زیبایی</Typography>
+
+        <Typography className={styles.bioTitle} variant='h6' color='text.secondary'>تخصص ها</Typography>
+        <Typography variant='body1'>درمان بیماریهای پوستی</Typography>
+        <Typography variant='body1'>لیزر درمانی</Typography>
+        <Typography variant='body1'>تزریق انواع ژل، مزوژل و بوتاکس</Typography>
+        <Typography variant='body1'>تزریق چربی (گرفت چربی)</Typography>
+        <Typography variant='body1'>جراحی‌های پوست</Typography>
+        <Typography variant='body1'>لیفت با نخ</Typography>
+
+        <Typography className={styles.bioTitle} variant='h6' color='text.secondary'>عضویت در مجامع حرفه ای</Typography>
+        <Typography variant='number'>عضو انجمن متخصصین پوست اروپا (EADV) از سال 2010 میلادی</Typography>
+        <Typography variant='number'>عضو انجمن متخصصین پوست ایران از سال 1376</Typography>
+
+        <Typography className={styles.bioTitle} variant='h6' color='text.secondary'>تحقیقات علمی</Typography>
+        <Typography variant='number'>شیوع PCO در بیمار زن مبتلا به آکنه در کنگره پوست استرالیا در سال 1376</Typography>
+
+        <Typography className={styles.bioTitle} variant='h6' color='text.secondary'>کنفرانس‌های علمی</Typography>
+        <Typography fontWeight='500' variant='body1'><b>Intense pulsed light therapy as an effective tool for reduction of pigmentation in macular amyloidosis patches</b>; EADV 2020</Typography>
+        <Typography fontWeight='500' variant='body1'><b>Intense Pulsed light laser treatment of facial papules in frontal fibrosing alopecia</b>; EADV 2019</Typography>
+        <Typography fontWeight='500'  variant='body1'><b>Post-inflammatory Hyperpigmentation Treatment with combination of fractional Q-switched laser plus carboxy therapy and glycolic peeling</b>; EADV 2018</Typography>
+        <Typography fontWeight='500' variant='body1'><b>Fractional 1064 nm Q-switched Nd-yag laser therapy as a safe and effective tool for reducing pigmentation in macular amyloidosis patches</b>; EADV 2017</Typography>
+        <Typography fontWeight='500' variant='body1'><b>Intense pulse light as a safe tool to remove melanocytic nevus</b>; EADV 2016</Typography>
+        <Typography fontWeight='500' variant='body1'><b>Treatment of Becker’s nevus with Intense pulse light</b>; WCD Vancouver 2015</Typography>
+        <Typography fontWeight='500' variant='body1'><b>Treatment of periorbital darkening with co2 fractional laser</b>; EADV 2013</Typography>
+
+        <Typography className={styles.fadeItem}>meow</Typography>
+        <Typography className={styles.fadeItem}>hi</Typography>
+
+      </Box>
     </Container>
   )
 }
