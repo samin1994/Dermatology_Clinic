158f978e0512d02c23e9ff39089bfa0e69c8cce7
samin1994 <saminlabbaf1994@gmail.com>
2025-05-20 02:29:47 -0400

    change logo path

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,12 +1,14 @@
 'use client'
 
 import * as React from 'react';
-import { Typography, Box, AppBar, Menu, MenuItem, Toolbar, Container,
-IconButton, Button } from '@mui/material'
+import {
+  Typography, Box, AppBar, Menu, MenuItem, Toolbar, Container,
+  IconButton, Button
+} from '@mui/material'
 import Image from 'next/image';
 import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link'
-import {menu} from '@/Constants'
+import { menu } from '@/Constants'
 import styles from './navbar.module.css'
 
 export default function Navbar() {
@@ -22,13 +24,13 @@ export default function Navbar() {
   };
 
   return (
-    <AppBar position='fixed' color='background' sx={{direction:'ltr', zIndex: 3, top: 0, borderRadius: 0}}>
+    <AppBar position='fixed' color='background' sx={{ direction: 'ltr', zIndex: 3, top: 0, borderRadius: 0 }}>
       <Box maxWidth="xl">
         <Toolbar disableGutters className={styles.container}>
           <Link href={'/'} passHref legacyBehavior>
-              <Box sx={{ cursor: 'pointer', display: { xs: 'none', md: 'flex' }, alignItems:'center', direction: 'ltr' }}>
-                <Image
-                src='/neg_vector.png'
+            <Box sx={{ cursor: 'pointer', display: { xs: 'none', md: 'flex' }, alignItems: 'center', direction: 'ltr' }}>
+              <Image
+                src='./neg_vector.png'
                 alt="Dr Negin Safdarian"
                 width={70}
                 height={70}
@@ -67,36 +69,36 @@ export default function Navbar() {
               sx={{
                 display: { xs: 'block', md: 'none' },
               }}
-            > 
-            {/* dropdown menu that is opened when size is mobile */}
-              {menu.map((menu) => { 
+            >
+              {/* dropdown menu that is opened when size is mobile */}
+              {menu.map((menu) => {
                 return (
                   <Link href={`${menu.link}`} key={menu.name} className={styles.link}>
-                  <MenuItem onClick={handleCloseNavMenu}>
-                    <Typography color='text.secondary' textAlign="center">{menu.name}</Typography>
-                  </MenuItem>
-                </Link>
+                    <MenuItem onClick={handleCloseNavMenu}>
+                      <Typography color='text.secondary' textAlign="center">{menu.name}</Typography>
+                    </MenuItem>
+                  </Link>
                 )
               })}
             </Menu>
           </Box>
           {/* logo that is shown in mobile size */}
           <Link href='/' className={styles.link}>
-            <Box sx={{ cursor: 'pointer', flexDirection: 'row-reverse', display: { xs: 'flex', md: 'none', direction: 'ltr' }, alignItems: 'center'}} >
+            <Box sx={{ cursor: 'pointer', flexDirection: 'row-reverse', display: { xs: 'flex', md: 'none', direction: 'ltr' }, alignItems: 'center' }} >
               <Box display='flex'>
                 <Typography ml={0.5} color='text.secondary'>Dr. </Typography>
                 <Typography ml={0.5} color='secondary.main'>Negin</Typography>
                 <Typography ml={0.5} color='text.secondary'>Safdarian</Typography>
               </Box>
               <Image
-                src='/neg_vector.png'
+                src='./neg_vector.png'
                 alt="Dr Negin Safdarian"
                 width={70}
                 height={70}
               />
             </Box>
           </Link>
-          <Box className={styles.menu} sx={{display: { xs: 'none', md: 'flex' }}}>
+          <Box className={styles.menu} sx={{ display: { xs: 'none', md: 'flex' } }}>
 
             {menu.map((menu) => (
               <Link
@@ -112,6 +114,6 @@ export default function Navbar() {
           </Box>
         </Toolbar>
       </Box>
-    </AppBar>  
+    </AppBar>
   )
 }
